82c160e37015714e9c766cfcd65db2922a4a8c32
Sree Teja <sreeteja.muthyala@gmail.com>
2018-05-05 16:21:48 +0530

    connected to firebase and tested it in the timetabledisplay activity

--- app/src/main/java/com/example/sreet/learning/timetabledisplay.java
+++ app/src/main/java/com/example/sreet/learning/timetabledisplay.java
@@ -2,13 +2,38 @@ package com.example.sreet.learning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.client.Firebase;
 
 public class timetabledisplay extends AppCompatActivity {
+
+    EditText myedittext;
+    Button myApplyBt;
+    String myString;
+    Firebase myfirebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("TimeTable");
         setContentView(R.layout.activity_timetabledisplay);
+
+        myedittext = (EditText) findViewById(R.id.editText);
+        myApplyBt = (Button) findViewById(R.id.button);
+        Firebase.setAndroidContext(this);
+        myfirebase = new Firebase("https://learning-2b334.firebaseio.com/");
+        myApplyBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myString = myedittext.getText().toString();
+                Firebase childbase = myfirebase.child("MychildName");
+                childbase.setValue(myString);
+                Toast.makeText(timetabledisplay.this, "success", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
