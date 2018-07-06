4040f7a1218f2d9442cbd4a3fed227541add7319
Sree Teja <sreeteja.muthyala@gmail.com>
2018-07-06 22:23:23 +0530

    Mid Way completion Commit

--- app/src/main/java/com/example/sreet/learning/FCMService.java
+++ app/src/main/java/com/example/sreet/learning/FCMService.java
@@ -6,6 +6,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -24,16 +25,16 @@ public class FCMService extends FirebaseMessagingService {
 
         // Check if message contains a data payload.
         //if (remoteMessage.getData().size() > 0) {
-          //  Log.d(TAG, "Message data payload: " + remoteMessage.getData().get("message"));
-            //dataTitle = remoteMessage.getData().get("title");
-            //dataMessage = remoteMessage.getData().get("message");
+        //  Log.d(TAG, "Message data payload: " + remoteMessage.getData().get("message"));
+        //dataTitle = remoteMessage.getData().get("title");
+        //dataMessage = remoteMessage.getData().get("message");
         //}
 
         // Check if message contains a notification payload.
         //if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-            notificationTitle = remoteMessage.getNotification().getTitle();
-            notificationBody = remoteMessage.getNotification().getBody();
+        Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        notificationTitle = remoteMessage.getNotification().getTitle();
+        notificationBody = remoteMessage.getNotification().getBody();
         //}
 
         // Also if you intend on generating your own notifications as a result of a received FCM
@@ -85,8 +86,10 @@ public class FCMService extends FirebaseMessagingService {
                 .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
                 .setWhen(System.currentTimeMillis())
                 .setAutoCancel(true)
+
                 //.setSound(defaultSoundUri);
                 .setContentIntent(pendingIntent);
+        if (Build.VERSION.SDK_INT >= 21) notificationBuilder.setVibrate(new long[0]);
 
 
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
